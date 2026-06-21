dc3f945269da1e6ac68026155287b890a4d21bc8
Sebastian Perez <sebastianperez@MacBook-Pro-2.local>
2026-06-20 22:47:48 -0400

    fix: hardcode sanity project id as fallback for build

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -3,9 +3,12 @@ import imageUrlBuilder from "@sanity/image-url";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type SanityImageSource = any;
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "re8ea6jc";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId,
+  dataset,
   apiVersion: "2024-01-01",
   useCdn: true,
 });
